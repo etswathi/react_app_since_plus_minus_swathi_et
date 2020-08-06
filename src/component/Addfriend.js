91fe831172e4acbb451a6d8474943c3d13212969
Swathi E T <Swathi.ET@cognizant.com>
2020-08-06 14:42:26 +0530

    form validations

--- src/component/Addfriend.js
+++ src/component/Addfriend.js
@@ -7,16 +7,51 @@ class AddFriend extends React.Component {
         super(props)
         this.state ={
             friendname:'',
-            friendsince:0
+            friendsince:0,
+            nameError:'',
+            sinceError:'',
+            buttonStatus: false
         }
+    }
+
+    checkValidation=()=>{
+       
+       let nameerror = ''
+       let sinceerror = ''
+       if(this.state.friendname.includes('#')){
+           console.log('name having #!');
+           nameerror = 'name having invalid #'
+       }
+       //check for other conditions!
+       if(nameerror){
+           console.log('set state for nameError');
+           this.setState({
+                nameError: nameerror,
+                buttonStatus: true
+            })
+           
+           return false
+       }
+
+       this.setState({
+           nameError:'',
+           buttonStatus:false
+        })
+       return true
     }
 
 
     getName=(event)=>{
+        
         console.log(event)
         console.log(event.target)
         console.log(event.target.value)
+        this.checkValidation()
         this.setState({friendname: event.target.value})
+        this.checkValidation()
+
+        
+       
 
     }
 
@@ -36,7 +71,7 @@ class AddFriend extends React.Component {
         axios.post('http://localhost:3000/allfriends', friendRequestBody)
                 .then(response=>{
                     console.log(response);
-                    this.props.history.push('/friends')
+                    this.props.history.push('/')
                 }, error=>{
                     console.error(error);
                 })
@@ -47,15 +82,17 @@ class AddFriend extends React.Component {
         
         return (
             <div>
-                <h3>Add Friend!!!!</h3>
+                <h3>Add New Friend!!!!</h3>
                 <form>
-                    <label>Name: </label>
+                    <label>Full Name: </label>
                     <input type='text' id="friendname" onChange={this.getName}></input>
+                    {this.state.nameError}
                     <br></br>
                     <label>Since: </label>
                     <input type='number' id="friendsince" onChange={this.getSince}></input>
+                    {this.state.sinceError}
                     <br></br>
-                    <button type="button" onClick={this.addFriend}>Add Friend</button>
+                    <button type="button" onClick={this.addFriend} disabled={this.state.buttonStatus}>Add Friend</button>
                     <br></br>
                     <div>
                         <h4>Preview</h4>
